fb4ac1c8f7e6587758317bd79c6b3a3a568f7c64
dev-tarun <tarun.yadav42009@gmail.com>
2026-08-27 18:19:32 +0530

    :wq

--- filetest.java
+++ filetest.java
@@ -9,6 +9,6 @@ public class filetest {
         File f1 = new File(args[0]);
         p(f1.getName()  + (f1.exists() ? " EXITS" : " DOES NOT EXITS"));
         p("File Size: " + f1.length() + " Bytes");
-        p("Is " + (f1.isDirectory() ? " A DIRECTORY" : " NOT A DIRECTORY"));
+        p("Is " + (f1.isDirectory() ? " A DIRECTORY" : " NOT A DIRECTORY"));  
     }
 }
